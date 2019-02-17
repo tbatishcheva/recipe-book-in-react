96d627c34abbb581be0f12da2371148198c0d23d
TatBat <tat.batishcheva@gmail.com>
2019-02-17 12:48:04 +0300

    1. Project Architecture

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1 className="sidebar__title">Recipe Book</h1>
+    );
+  }
+}
+
+export default App;
